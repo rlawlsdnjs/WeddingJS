126befa9c76f79824f17e272454da791667b9364
JinWon <dlfjs9514@nate.com>
2025-03-13 20:31:52 +0900

    Rename : sj - js

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weddingSJ/', // GitHub 저장소 이름으로 설정
+  base: '/weddingJS/', // GitHub 저장소 이름으로 설정
   plugins: [react()],
 })
